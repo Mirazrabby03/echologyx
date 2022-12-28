3efd9af828947c2e6ef1cde299e688885b897b44
Miraz Khan <mirazrabby03@gmail.com>
2022-12-28 21:59:20 +0600

    last commit on 28-12-2022

--- src/Shadow Test/Mini Cart/V-01/index.js
+++ src/Shadow Test/Mini Cart/V-01/index.js
@@ -68,47 +68,151 @@ var echoVariation = {
                 />
             </div>
             <div class='cart_item--info'>
-            <div class="cross ">
-                <img src="https://cdn.shopify.com/s/files/1/0495/0596/0091/files/Vector_2.svg?v=1629899010"/>
+            <div class='cross '>
+                <img src='https://cdn.shopify.com/s/files/1/0495/0596/0091/files/Vector_2.svg?v=1629899010' />
+            </div>
+            <div class='cart_item--details'>
+                <a href='${url}'>
+                    <p class='text'>
+                        <span> ${title}</span>
+                    </p>
+                </a>
+            </div>
+        
+            <div class='cart_item--priceDetails'>
+                <div class='deal '>
+                    <span class='currentPrice'>
+                        ${dollar}${totalPrice}
+                    </span>
                 </div>
-                <div class='cart_item--details'>
-                    <a href='${url}'>
-                        <p class='text'>
-                             <span> ${title}</span>
-                        </p>
-                    </a>
-                </div>
-                
-                <div class='cart_item--priceDetails'>
-                    <div class='deal '>
-                        <span>${dollar}${totalPrice}</span>
+            </div>
+        
+            <div class='cart_item--action'>
+                <div class='cart_item--actionQuantity'>
+                    <div class="counter_area" >
+                        <span class= "minus" > - </span>
+                        <span class="currentItem">${quantity}</span>
+                        <span class="pluse">+</span>
                     </div>
                 </div>
-
-                <div class='cart_item--action'>
-                <div class='cart_item--actionQuantity'>
-                   
-                    <div class="counter_area >
-                    <span class="minus">-</span>
-                    <span class="currentItem">${quantity}</span>
-                    <span class="pluse">+</span>
-                </div>
-                    
-                </div>
             </div>
+            
+        </div>
             </div>
-            </div>
-           </div>
+           
        
             `);
     
         })
+        document.querySelector('.cart_item_container').insertAdjacentHTML('beforeend', `
+        <div class="checkout-button"><a href="/cart" class="btn animation btn-dark btn-go-to-cart">checkout</a>
+            </div>
+        `);
+
+        // window.addEventListener('click', function(e){   
+        //     if (document.querySelector('.cart_item_container').contains(e.target)){
+        //       console.log('You clicked inside the box')
+        //     } else{
+        //         this.document.querySelector('.cart_item_container').remove();
+        //     }
+        //   })
+
+
           document.querySelectorAll('.pluse').forEach(elm=>{
                     elm.addEventListener('click', function(e){
                         e.preventDefault();
                         var informationData = e.target.closest('.cart_image_item_container')
                         console.log("information data",informationData)
+        
+
+                        var currentQuantity =  informationData.getAttribute("data-quantity");
+                        var actualPrice = informationData.getAttribute('data-variant-price');
+                        var body = "%5B" + informationData.getAttribute('data-item-key').replace(":","%3A") + "%5D=" + (+currentQuantity + 1) ;
+                        console.log(body);
+                        var sku = informationData.getAttribute("data-sku")
+                        console.log('sku',sku)
+                        var cartSku = document.querySelector('.cart_item').getAttribute('data-sku');
+                        console.log('cartSku', cartSku)
+                        if(cartSku == sku){
+
+                        }
+                        fetch("https://loveamika.com/cart/update.js", {
+                            "headers": {
+                                
+                                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                                
+                                "x-requested-with": "XMLHttpRequest"
+                            },
+                            "referrer": "https://loveamika.com/",
+                            "referrerPolicy": "strict-origin-when-cross-origin",
+                            
+
+                            "body": `updates${body}`,
+                            "method": "POST",
+                            "mode": "cors",
+                            "credentials": "include"
+                            })
+                            .then((response) => response.json())
+                            .then((data) => {
+                                let currentQuantity = e.target.closest('.cart_item--actionQuantity').querySelector('.currentItem').textContent;
+                                e.target.closest('.cart_item--actionQuantity').querySelector('.currentItem').textContent = +currentQuantity + 1; 
+
+                                let currentPrice = e.target.closest('.cart_image_item_container').querySelector('.currentPrice').textContent;
+                                let onlyPrice = currentPrice.replace(/[^\d.]/g, '')
+                                var priceTag = e.target.closest('.cart_image_item_container').querySelector('.currentPrice').textContent = parseInt(onlyPrice) + parseInt(actualPrice);
+                                e.target.closest('.cart_image_item_container').querySelector('.currentPrice').textContent = `${dollar}${priceTag}`
+
+
+                                console.log("this is data", data);
+                            })
+                            .catch((error) => {
+                                console.error('Error:', error);
+                            });
+
+                            var dataVariant =  informationData.getAttribute("data-variant");
+                        
+
+                            // fetch("https://loveamika.com/cart/add.js", {
+                            //     "headers": {
+                                    
+                            //         "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                                    
+                            //         "x-requested-with": "XMLHttpRequest"
+                            //     },
+                            //     "referrer": "https://loveamika.com/",
+                            //     "referrerPolicy": "strict-origin-when-cross-origin",
+                                
+    
+                            //     "body": `quantity=1&id=${dataVariant}`,
+                            //     "method": "POST",
+                            //     "mode": "cors",
+                            //     "credentials": "include"
+                            //     })
+                            //     .then((response) => response.json())
+                            //     .then((data) => {
+                                  
+                            //         console.log("this is data", data);
+                            //     })
+                            //     .catch((error) => {
+                            //         console.error('Error:', error);
+                            //     });
+               
+                    })
+                })
+
+
+                document.querySelectorAll('.minus').forEach(elm=>{
+                    elm.addEventListener('click', function(e){
+                        e.preventDefault();
+                        var informationData = e.target.closest('.cart_image_item_container')
+                        console.log("information data",informationData)
                         var dataVariant =  informationData.getAttribute("data-variant");
+
+                        var currentQuantity =  informationData.getAttribute("data-quantity");
+                        var actualPrice = informationData.getAttribute('data-variant-price');
+                        var parsedPrice = parseInt(actualPrice);
+                        var bodyMinus = "%5B" + informationData.getAttribute('data-item-key').replace(":","%3A") + "%5D=" + (+currentQuantity - 1) ;
+                        console.log(bodyMinus);
                    
                         fetch("https://loveamika.com/cart/update.js", {
                             "headers": {
@@ -119,7 +223,9 @@ var echoVariation = {
                             },
                             "referrer": "https://loveamika.com/",
                             "referrerPolicy": "strict-origin-when-cross-origin",
-                            "body": `quantity=1&id=${dataVariant}`,
+                            
+
+                            "body": `updates${bodyMinus}`,
                             "method": "POST",
                             "mode": "cors",
                             "credentials": "include"
@@ -127,7 +233,13 @@ var echoVariation = {
                             .then((response) => response.json())
                             .then((data) => {
                                 let currentQuantity = e.target.closest('.cart_item--actionQuantity').querySelector('.currentItem').textContent;
-                                e.target.closest('.cart_item--actionQuantity').querySelector('.currentItem').textContent = +currentQuantity + 1; 
+                                e.target.closest('.cart_item--actionQuantity').querySelector('.currentItem').textContent = +currentQuantity - 1; 
+
+                                let currentPrice = e.target.closest('.cart_image_item_container').querySelector('.currentPrice').textContent;
+                                let onlyPrice = currentPrice.replace(/[^\d.]/g, '')
+                                var priceTag = e.target.closest('.cart_image_item_container').querySelector('.currentPrice').textContent = parseInt(onlyPrice) - parseInt(actualPrice);
+                                e.target.closest('.cart_image_item_container').querySelector('.currentPrice').textContent = `${dollar}${priceTag}`
+                                console.log("this is data", data);
                             })
                             .catch((error) => {
                                 console.error('Error:', error);
@@ -135,8 +247,43 @@ var echoVariation = {
                
                     })
                 })
+
+                document.querySelectorAll('.cross').forEach((elm) => {
+                    elm.addEventListener('click', function(e){
+                        e.preventDefault();
+                        var informationData = e.target.closest('.cart_image_item_container')
+                        var removeItem = "%5B" + informationData.getAttribute('data-item-key').replace(":","%3A") + "%5D=" + 0 ;
+                        fetch("https://loveamika.com/cart/update.js", {
+                            "headers": {
+                                
+                                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                                
+                                "x-requested-with": "XMLHttpRequest"
+                            },
+                            "referrer": "https://loveamika.com/",
+                            "referrerPolicy": "strict-origin-when-cross-origin",
+                            
+
+                            "body": `updates${removeItem}`,
+                            "method": "POST",
+                            "mode": "cors",
+                            "credentials": "include"
+                            })
+                            .then((response) => response.json())
+                            .then((data) => {
+                               e.target.closest('.cart_image_item_container').remove();
+                                console.log("this is data", data);
+                            })
+                            .catch((error) => {
+                                console.error('Error:', error);
+                            });
+
+                    })
+                })
     
      	}
+
+        
 };
 
     
@@ -163,4 +310,11 @@ var echoVariation = {
                         Remove
                     </a>
                 <input readonly value = ${quantity} />
-                */}
+
+
+                "body": `quantity=1&id=${dataVariant}`,
+
+                document.querySelector("#page > div.side-cart.active > div > div.wrap > div:nth-child(3) > form > div:nth-child(4) > div.cart_item--action > div > select > option:nth-child(1)").setAttribute("selected",true)
+               
+            document.querySelector('.cart_item[data-sku="AM50.18094"]')
+            */}
